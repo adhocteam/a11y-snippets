40be4add3fcd2f931cc41a2dd3e7c7c76d41a260
1Copenut <trevor@adhocteam.us>
2021-09-10 11:30:30 -0500

    Removing Next specific files from commit

--- test-scripts/CYPRESS-accessible-button-labels/.next/static/P990J90kAtMAENRtH3qm7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5dd3a863eec8dc33b66f.css","static/chunks/pages/index-8acc1476a01f51b653c4.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
